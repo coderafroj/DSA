bae73b68ea674632e15669278d7ed52de4e48ef8
coderafroj <coderafroj@gmail.com>
2026-04-15 06:59:03 +0530

    add

---
+++ questions/patterns/t4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+// 1 1 1 1 
+//   2 2 2
+//     3 3
+//       4
+int main(){
+    int n=4;
+    for(int i=0;i<n;i++){
+        // space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        //number
+        for(int j=0;j<n-i;j++){
+            cout<<i+1;
+        }
+        cout<<endl;
+    }
+
+}
--- questions/patterns/t5.cpp
+++ questions/patterns/t5.cpp
@@ -1,10 +1,14 @@
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
 #include <iostream>
 using namespace std;
 int main(){
-    int n=5;
+    int n=4;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<i<<" ";
+        for(int j=i;j>0;j--){
+            cout<<j<<" ";
         }
         cout<<endl;
     }
